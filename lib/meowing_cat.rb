80d3eef36fec585df1a2b89b562028a802ac208b
sarah <s.j.condon01@gmail.com>
2019-09-29 02:12:31 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,13 @@
 ## code your solution here. 
-class catch
-    attr_accessor :name
+class cat
+    attr_accessor :meow
+    meow = cat.new
+cat.meow = "Shawn Carter"
+jay_z.name
  end
+ 
+ jay_z = Person.new
+jay_z.name = "Shawn Carter"
+jay_z.name
  
  
