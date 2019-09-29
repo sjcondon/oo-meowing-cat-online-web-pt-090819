c8b74779384604bb4f761e3d1e5106d0b5c3d366
sarah <s.j.condon01@gmail.com>
2019-09-29 02:58:52 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,6 @@
 class Cat
   maru = Cat.new
-
-  
-  maru.name = "Maru"
+maru.name = "Maru"
   maru.name
   
   attr_accessor :name
@@ -12,9 +10,18 @@ end
  def meow=(meow)
 @meow = meow
 end
-
+maru.name
 maru.meow
 puts "meow!"
+
+fido = Dog.new
+fido.name = "Fido"
+ 
+fido.name
+# => "Fido"
+ 
+fido.bark
+woof!
 
 
   
