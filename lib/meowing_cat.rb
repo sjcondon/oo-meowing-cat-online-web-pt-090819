7bba6f458eb747fc4a37a3036570912700f619c8
sarah <s.j.condon01@gmail.com>
2019-09-29 02:13:18 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,13 +1,8 @@
 ## code your solution here. 
 class cat
     attr_accessor :meow
-    meow = cat.new
-cat.meow = "Shawn Carter"
-jay_z.name
- end
- 
- jay_z = Person.new
-jay_z.name = "Shawn Carter"
-jay_z.name
- 
+    jay_z = Person.new
+jay_z.meow = "Shawn Carter"
+jay_z.
+  => "Shawn Carter"
  
